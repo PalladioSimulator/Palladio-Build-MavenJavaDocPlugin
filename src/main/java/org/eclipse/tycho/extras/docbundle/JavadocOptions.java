82d706b1cf77a51e78a696bc259b6a799a9a6da6
Sebastian Krach <krach@fzi.de>
2021-01-14 08:48:42 +0100

    Sync with latest version for tycho 2.2.0

--- src/main/java/org/eclipse/tycho/extras/docbundle/JavadocOptions.java
+++ src/main/java/org/eclipse/tycho/extras/docbundle/JavadocOptions.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2013, 2015 IBH SYSTEMS GmbH and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     IBH SYSTEMS GmbH - initial API and implementation
@@ -16,13 +18,9 @@ import java.util.List;
 import org.apache.maven.model.Dependency;
 
 /**
- * <p>
- * The javadoc options
- * </p>
- * <p>
+ * The javadoc options<br>
  * At the moment the list of real options is quite small, but most arguments can be passed using the
  * <code>additionalArguments</code> property.
- * </p>
  * 
  * @author Jens Reimann
  */
--- src/test/java/org/eclipse/tycho/extras/docbundle/AssertOnBuffer.java
+++ src/test/java/org/eclipse/tycho/extras/docbundle/AssertOnBuffer.java
@@ -1,16 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2015 VDS Rail and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2015, 2020 VDS Rail and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    Enrico De Fent - initial API and implementation
  *******************************************************************************/
 package org.eclipse.tycho.extras.docbundle;
 
-import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class AssertOnBuffer {
     final String[] lines;
@@ -20,25 +23,25 @@ class AssertOnBuffer {
     public AssertOnBuffer(String buffer) {
         this.buffer = buffer;
 
-        String lineSeparator = System.getProperty("line.separator");
+        String lineSeparator = System.lineSeparator();
         this.lines = buffer.split(lineSeparator);
     }
 
     public void assertMoreLines() {
         if (curLine == lines.length) {
-            Assert.fail("expected more lines, found no more");
+            fail("expected more lines, found no more");
         }
     }
 
     public void assertNextLine(String line) {
         assertMoreLines();
-        Assert.assertEquals("at line " + curLine, line, lines[curLine]);
+        assertEquals(line, lines[curLine], "at line " + curLine);
         curLine++;
     }
 
     public void assertNoMoreLines() {
         if (curLine < lines.length) {
-            Assert.fail(String.format("expected no more lines, found %d more", lines.length - curLine));
+            fail(String.format("expected no more lines, found %d more", lines.length - curLine));
         }
     }
 }
